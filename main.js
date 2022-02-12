b139260f80cf4c54e4829b4f9cad95fe2612b0ec
CayoRoberto <cayoroberto08@gmail.com>
2022-02-12 09:22:15 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+function Enviar(){
+  //var nome = document.getElementById("usuarioNome");
+  //var email = document.getElementById("usuarioEmail");
+  //var mensagem = document.getElementById("usuarioMensagem");
+
+  var nome = formcontato.nome.value;
+ 
+  
+
+  print("Estou aqui");
+  //var dados = localStorage.getItem(dadosUsuario)
+
+  if(nome != "" ){
+    
+    alert('Obrigado sr(a) ' + nome + " a sua mensagem foi encaminhada para nossa equipe com sucesso");
+  }
+  
+}
